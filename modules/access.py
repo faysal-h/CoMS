115d397e3346039e26d0116efeccc7f1607fb542
faysal hanif <faysal hanif@hotmail.com>
2022-03-17 22:32:04 +0500

    classify ACCESS Module and removed unnecessary files

--- modules/access.py
+++ modules/access.py
@@ -1,3 +1,4 @@
+from msilib.schema import tables
 import os
 import pandas as pd
 import pyodbc
@@ -32,18 +33,59 @@ class AccessFile():
 
 
     def readQuery(self, Query):
-        return pd.read_sql_query(Query, self.cnxn)
+        df = pd.read_sql_query(Query, self.cnxn)
+        if(df.empty):
+            print("No data found against this case number.")
+        else:
+            print("Data found against this case number.")
+            return df
+
+
+
+class Tables():
+    def __init__(self, ftmNo) -> None:
+        self.ftmNo = ftmNo
+        self.database = AccessFile()
+        self.cocDF = self.getCOCtable()
+
+    def getCOCtable(self):
+
+        queryCOC = f''' SELECT COC.[caseFTMFK], COC.[frmGRLDate], COC.[ProcessingDate], COC.[ComparisonStartDate], 
+                        COC.[ComparisonCompDate], COC.[ReviewStartDate], COC.[ReviewEndDate], COC.[BalScanStartDate], 
+                        COC.[BalScanCompDate], COC.[toCPRDate]
+                        FROM COC
+                        WHERE (((COC.[caseFTMFK])={self.ftmNo}));
+
+                    '''
+        return self.database.readQuery(queryCOC)
+
+
+
+    def getCaseDetailsTable(self):
+        queryCaseDetails = f''' SELECT CaseDetails.[caseYear], CaseDetails.[casePFSA], CaseDetails.[caseFTM], 
+                                CaseDetails.[NoOfParcels], CaseDetails.[AnalystName], CaseDetails.[ReviewerName]
+                                FROM CaseDetails
+                                WHERE (((CaseDetails.[caseFTM])={self.ftmNo}));'''
+        return self.database.readQuery(queryCaseDetails)
+
+    def getParcelsTable(self):
+        queryParcelsDetails = f''' SELECT Parcel.CaseNoFK, Parcel.ParcelNo, Parcel.SubmissionDate, Parcel.SubmitterName, 
+                                Parcel.Rank, Items.FIR, Items.FIRDate, Items.EVCaliber, 
+                                Items.EVType, Items.EV, Items.ItemNo, Items.Quantity
+                                FROM Parcel INNER JOIN Items ON Parcel.[ID] = Items.[ParcelNoFK]
+                                WHERE (((Parcel.CaseNoFK)={self.ftmNo}));
+                                '''
+        return self.database.readQuery(queryParcelsDetails)
         
 
-class DataFrames():
-    pass
+    def getDate(self, whichDate):
+        return self.cocDF.iloc[0][whichDate].to_pydatetime()
 
 
-    
+if __name__ == "__main__":
 
 
-db = AccessFile()
-
-
-df = db.readQuery(queryFtmNumber)
-print(df) 
+    db = Tables("123456")
+    print(db.getCOCtable())
+    print(db.getCaseDetailsTable())
+    print(db.getParcelsTable())
