7713e0d5bd0f6a3d539ca2efc6d41839a9d96874
faysal hanif <faysal hanif@hotmail.com>
2022-03-20 14:19:25 +0500

    Modified and finalized changes (lots of)

--- modules/access.py
+++ modules/access.py
@@ -1,16 +1,32 @@
-from msilib.schema import tables
 import os
+from datetime import datetime
+
 import pandas as pd
 import pyodbc
 
 dbPath = os.path.join(os.getcwd(), "CMSdatabase.accdb")
 
-queryFtmNumber =    '''SELECT CaseDetails.caseYear, CaseDetails.casePFSA, CaseDetails.caseFTM, CaseDetails.NoOfParcels, CaseDetails.FrmGRLdate,
-                        Parcel.ParcelNo, Parcel.SubmissionDate, Parcel.SubmitterName, Parcel.Rank, 
-                        Items.FIR, Items.FIRDate, Items.EVCaliber, Items.EVType, Items.EV, Items.ItemNo, Items.Quantity
-                        FROM (CaseDetails INNER JOIN Parcel ON CaseDetails.caseFTM = Parcel.CaseNoFK) INNER JOIN Items ON Parcel.ID = Items.ParcelNoFK;
-                    '''
 
+
+queryCaseDetails = '''SELECT CaseDetails.[caseYear], CaseDetails.[casePFSA], CaseDetails.[caseFTM], CaseDetails.[CaseNosAddl],
+                        CaseDetails.[NoOfParcels], CaseDetails.[AnalystName], CaseDetails.[ReviewerName]
+                        FROM CaseDetails
+                        WHERE (((CaseDetails.[caseFTM])=
+                        '''
+queryParcelsDetails = '''SELECT Parcel.CaseNoFK, Parcel.ParcelNo, Parcel.SubmissionDate, Parcel.SubmitterName, 
+                        Parcel.Rank, Items.FIR, Items.FIRDate, Items.EVCaliber, 
+                        Items.EVType, Items.EV, Items.ItemNo, Items.Quantity
+                        FROM Parcel INNER JOIN Items ON Parcel.[ID] = Items.[ParcelNoFK]
+                        WHERE (((Parcel.CaseNoFK)=
+                        '''
+queryCOC = '''SELECT COC.[caseFTMFK], COC.[frmGRLDate], COC.[ProcessingDate], COC.[ComparisonStartDate], 
+                COC.[ComparisonCompDate], COC.[ReviewStartDate], COC.[ReviewEndDate], COC.[BalScanStartDate], 
+                COC.[BalScanCompDate], COC.[toCPRDate]
+                FROM COC
+                WHERE (((COC.[caseFTMFK])='''
+
+
+#TODO Need to change Connectable ENGINE to SQLAlchemy
 class AccessFile():
     def __init__(self) -> None:
         self.openConnection()
@@ -31,61 +47,63 @@ class AccessFile():
     def closeConnection(self):
         self.cnxn.close()
 
-
     def readQuery(self, Query):
         df = pd.read_sql_query(Query, self.cnxn)
         if(df.empty):
-            print("No data found against this case number.")
+            print("Reading Query Failure. No data found against this case number.")
         else:
-            print("Data found against this case number.")
+            print("Reading Query Success. Data found against this case number.")
             return df
-
-
 
 class Tables():
     def __init__(self, ftmNo) -> None:
         self.ftmNo = ftmNo
         self.database = AccessFile()
-        self.cocDF = self.getCOCtable()
 
-    def getCOCtable(self):
-
-        queryCOC = f''' SELECT COC.[caseFTMFK], COC.[frmGRLDate], COC.[ProcessingDate], COC.[ComparisonStartDate], 
-                        COC.[ComparisonCompDate], COC.[ReviewStartDate], COC.[ReviewEndDate], COC.[BalScanStartDate], 
-                        COC.[BalScanCompDate], COC.[toCPRDate]
-                        FROM COC
-                        WHERE (((COC.[caseFTMFK])={self.ftmNo}));
-
-                    '''
-        return self.database.readQuery(queryCOC)
-
-
-
-    def getCaseDetailsTable(self):
-        queryCaseDetails = f''' SELECT CaseDetails.[caseYear], CaseDetails.[casePFSA], CaseDetails.[caseFTM], 
-                                CaseDetails.[NoOfParcels], CaseDetails.[AnalystName], CaseDetails.[ReviewerName]
-                                FROM CaseDetails
-                                WHERE (((CaseDetails.[caseFTM])={self.ftmNo}));'''
-        return self.database.readQuery(queryCaseDetails)
-
-    def getParcelsTable(self):
-        queryParcelsDetails = f''' SELECT Parcel.CaseNoFK, Parcel.ParcelNo, Parcel.SubmissionDate, Parcel.SubmitterName, 
-                                Parcel.Rank, Items.FIR, Items.FIRDate, Items.EVCaliber, 
-                                Items.EVType, Items.EV, Items.ItemNo, Items.Quantity
-                                FROM Parcel INNER JOIN Items ON Parcel.[ID] = Items.[ParcelNoFK]
-                                WHERE (((Parcel.CaseNoFK)={self.ftmNo}));
-                                '''
-        return self.database.readQuery(queryParcelsDetails)
+    def getTable(self, queryToRead:str) -> pd.DataFrame:
+        return self.database.readQuery(f"{queryToRead} {self.ftmNo}));")
         
+class CaseDetails(Tables):
+    def __init__(self, ftmNo) -> None:
+        super().__init__(ftmNo)
+        self.caseDetailsDF = self.getTable(queryCaseDetails)
 
-    def getDate(self, whichDate):
+    def getCaseNoParts(self) -> tuple:
+        return self.caseDetailsDF.iloc[0]['caseYear'], self.caseDetailsDF.iloc[0]['casePFSA'], self.caseDetailsDF.iloc[0]['caseFTM'] 
+
+    def getValuefrmCaseDetails(self, columnName, indexNumber=0):
+        return self.caseDetailsDF.iloc[indexNumber][columnName]
+
+
+class COC(Tables):
+    def __init__(self, ftmNo) -> None:
+        super().__init__(ftmNo)
+        self.cocDF = self.getTable(queryCOC)
+    
+    def getCOCdate(self, whichDate : str) -> datetime:
         return self.cocDF.iloc[0][whichDate].to_pydatetime()
+
+
+class Parcels(Tables):
+    def __init__(self, ftmNo) -> None:
+        super().__init__(ftmNo)
+        self.parcelsDF = self.getTable(queryParcelsDetails)
+
+    # Filters and Slices dataframe
+    def getFirearmsOrAmmoDF(self, typeOfItems:list) -> pd.DataFrame:
+        df1 = self.parcelsDF[self.parcelsDF['EVType'].isin(typeOfItems)]
+        return df1[['ParcelNo', 'EVCaliber', 'EVType', 'EV', 'ItemNo', 'Quantity']]
+
+    def getNoOfParcels(self) -> int:
+        return len(self.parcelsDF.index)
+
+    def getValuefrmParcels(self, columnName, indexNumber):
+        return self.caseDetailsDF.iloc[indexNumber][columnName]
+
+
 
 
 if __name__ == "__main__":
 
-
-    db = Tables("123456")
-    print(db.getCOCtable())
-    print(db.getCaseDetailsTable())
-    print(db.getParcelsTable())
+    d = CaseDetails(123456)
+    print(d.caseDetailsDF)
