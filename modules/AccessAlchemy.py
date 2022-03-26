72df62a1a2245bd7c9b247e87f99e97082dedfd2
Faysal Hanif <faysalhanif@hotmail.com>
2022-03-26 12:58:23 +0500

    Added SqlAlchemy-Access functionlity. Need to test it tough

---
+++ modules/AccessAlchemy.py
@@ -0,0 +1,22 @@
+import os
+import urllib
+import pandas as pd
+from sqlalchemy import create_engine
+
+dbPath = os.path.join(os.getcwd(), "CMSdatabase.accdb")
+
+connection_string = (
+    r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};"
+    f"DBQ={dbPath};"
+    r"ExtendedAnsiSQL=1;"
+)
+connection_uri = f"access+pyodbc:///?odbc_connect={urllib.parse.quote_plus(connection_string)}"
+engine = create_engine(connection_uri)
+
+queryCaseDetails = '''SELECT CaseDetails.[caseYear], CaseDetails.[casePFSA], CaseDetails.[caseFTM], CaseDetails.[CaseNosAddl],
+                        CaseDetails.[NoOfParcels], CaseDetails.[AnalystName], CaseDetails.[ReviewerName], CaseDetails.[TestsRequest], 
+                        CaseDetails.[Balscanner], CaseDetails.[TeamMember], CaseDetails.[Addressee]
+                        FROM CaseDetails
+                        '''
+
+df = pd.read_sql_query(queryCaseDetails, engine)
