f481a90ebf3f4896b9d2376b603b91b2528a7db5
Faysal Hanif <faysalhanif@hotmail.com>
2022-03-26 18:08:10 +0500

    SqlAlchemy Access Test module added

--- modules/AccessAlchemy.py
+++ modules/AccessAlchemy.py
@@ -1,22 +1,38 @@
-import os
-import urllib
-import pandas as pd
-from sqlalchemy import create_engine
+# from operator import itemgetter
+# import os
+# import urllib
+# import pandas as pd
+# from sqlalchemy import create_engine
 
-dbPath = os.path.join(os.getcwd(), "CMSdatabase.accdb")
+# dbPath = os.path.join(os.getcwd(), "CMSdatabase.accdb")
 
-connection_string = (
-    r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};"
-    f"DBQ={dbPath};"
-    r"ExtendedAnsiSQL=1;"
-)
-connection_uri = f"access+pyodbc:///?odbc_connect={urllib.parse.quote_plus(connection_string)}"
-engine = create_engine(connection_uri)
+# connection_string = (
+#     r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};"
+#     f"DBQ={dbPath};"
+#     r"ExtendedAnsiSQL=1;"
+# )
+# connection_uri = f"access+pyodbc:///?odbc_connect={urllib.parse.quote_plus(connection_string)}"
+# engine = create_engine(connection_uri)
 
-queryCaseDetails = '''SELECT CaseDetails.[caseYear], CaseDetails.[casePFSA], CaseDetails.[caseFTM], CaseDetails.[CaseNosAddl],
-                        CaseDetails.[NoOfParcels], CaseDetails.[AnalystName], CaseDetails.[ReviewerName], CaseDetails.[TestsRequest], 
-                        CaseDetails.[Balscanner], CaseDetails.[TeamMember], CaseDetails.[Addressee]
-                        FROM CaseDetails
-                        '''
+# queryCaseDetails = '''SELECT CaseDetails.[caseYear], CaseDetails.[casePFSA], CaseDetails.[caseFTM], CaseDetails.[CaseNosAddl],
+#                         CaseDetails.[NoOfParcels], CaseDetails.[AnalystName], CaseDetails.[ReviewerName], CaseDetails.[TestsRequest], 
+#                         CaseDetails.[Balscanner], CaseDetails.[TeamMember], CaseDetails.[Addressee]
+#                         FROM CaseDetails
+#                         '''
 
-df = pd.read_sql_query(queryCaseDetails, engine)
+# df = pd.read_sql_query(queryCaseDetails, engine)
+# print(df)
+
+# # Test if access driver engine installed
+# import pyodbc
+# [x for x in pyodbc.drivers() if x.startswith('Microsoft Access Driver')]
+
+itemNo = 'R1'
+itemsToCheck = {'R':f'{itemNo}tc', 'P': 'tc', 'S': 'ts', 'r': 'tc', 'p': 'tc', 's': 'ts'}
+
+
+# if any(ext in itemNo for ext in itemsToCheck):
+#     print(itemNo)
+for key in itemsToCheck:
+    if(itemNo.find(key) != -1):
+        print(itemsToCheck[key])
