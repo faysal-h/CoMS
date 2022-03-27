9d2c9a59db772160888948db4d1d4f9b0922b937
Faysal Hanif <faysalhanif@hotmail.com>
2022-03-27 14:42:50 +0500

    Created Main class and First beta build

---
+++ main.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+import sys
+import logging
+from dateutil.parser import parse
+
+import pymsgbox
+
+from modules.AccessToDF import DataFrames
+from modules import DocxEngine
+
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+class menu():
+    def __init__(self) -> None:
+         self.choices = {
+                        'Generate Sheets' : self.getCaseNoFromUser,
+                        'Generate Identifiers' : self.getBatchDateFromUser,
+                        "Quit": self.quitCMS
+                        }
+
+    def numericORlengthWarning(self):
+        pymsgbox.alert(text="Enter a VALID 5 or 6 digits FTM number only.",
+                            title="What are you doing?", button="Ok. I'm sorry")
+
+    def getCaseNoFromUser(self):
+        ftmNo = pymsgbox.prompt(text='Enter FTM number', title="Sheet Generator")
+        
+        if(ftmNo is None):
+            self.run
+        else:
+            if(self.validateCaseNumber(ftmNo)):
+                self.generateSheets(ftmNo)
+   
+    def getBatchDateFromUser(self):
+        batchDate = pymsgbox.prompt(text='Enter Batch Dat', title="Identifiers Generator")
+        
+        if(batchDate is None):
+            self.run
+        else:
+            if(self.is_date(batchDate) and DataFrames(ftmNo="").checkIfBatcDateExist(BatchDate=batchDate)):
+                self.generateIdentifiers(batchDate=batchDate)
+            else:
+                pymsgbox.alert(text="Enter valid date or Date does Not exist in Database.", title="Warning")
+    
+    def userPrompt(self):
+        # return pymsgbox.prompt(text="Enter FTM Number for sheets.\nEnter BATCH DATE for Identifiers", title='CMS')
+        return pymsgbox.confirm(text="What do you want to do?", title='CMS', 
+                                buttons=[   'Generate Sheets', 'Generate Identifiers',
+                                            'Quit'
+                                        ]
+                                )
+
+    def run(self):
+        while(True):
+            choice = self.userPrompt()
+            action = self.choices.get(choice)
+            if action:
+                action()
+            else:
+                pymsgbox.alert(text="Not a valid choice", title='Warning')
+
+    def quitCMS(self):
+        sys.exit(0)
+    
+    def validateCaseNumber(self, ftmNumber:int):
+        if(ftmNumber.isnumeric()):
+            if(len(str(ftmNumber)) in [5, 6]):
+                if(DataFrames(ftmNumber).checkIfCaseExist()):
+                    return True
+                else:
+                    self.numericORlengthWarning()
+            else:
+                self.numericORlengthWarning()
+        else:
+            self.numericORlengthWarning()
+
+    def is_date(self, batchDate, fuzzy=False):
+        """
+        Return whether the string can be interpreted as a date.
+
+        :param string: str, string to check for date
+        :param fuzzy: bool, ignore unknown tokens in string if True
+        """
+        try: 
+            parse(batchDate, fuzzy=fuzzy)
+            return True
+
+        except ValueError:
+            return False
+
+    def generateSheets(self, ftmNumber):
+        DocxEngine.ProcessingSheetProcessor(ftmNumber=ftmNumber).proceesingSheetMaker()
+        DocxEngine.FirearmsProcessor(ftmNumber=ftmNumber).firearmSheetMaker()
+        DocxEngine.CartridgeProcessor(ftmNumber=ftmNumber).cartridgeSheetMaker()
+        DocxEngine.BulletProcessor(ftmNumber=ftmNumber).bulletSheetMaker()
+        saveLocation = DocxEngine.ReportProcessor(ftmNumber=ftmNumber).reportGenerator()
+        pymsgbox.alert(text=f"All sheets are generated.", title="Success")
+
+    def generateIdentifiers(self, batchDate):        
+        DocxEngine.IdentifiersProcessor(batchDate).FileIdentifierMaker()
+        DocxEngine.IdentifiersProcessor(batchDate).EnvelopsMaker()
+
+if __name__ == "__main__":
+    menu().run()
